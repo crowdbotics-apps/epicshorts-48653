e77ad6ff35e2fddddaf26e93e3b51ba4af1d9329
Crowdbotics <team@crowdbotics.com>
2024-07-05 22:25:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "epicshorts-48653",
+  url: "https://epicshorts-48653.botics.co",
+  api: "https://epicshorts-48653.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
